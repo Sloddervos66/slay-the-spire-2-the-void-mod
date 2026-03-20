97b33c1ccf603e950d0682d0805032e518b7fd78
Sloddervos66 <sloddervos66@gmail.com>
2026-03-20 20:13:22 +0100

    Made patch touch of orobas better

---
+++ TheVoidCode/Patches/PatchTouchOfOrobas.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Relics;
+using TheVoid.TheVoidCode.Relics;
+
+namespace TheVoid.TheVoidCode.Patches;
+
+[HarmonyPatch(typeof(TouchOfOrobas), nameof(TouchOfOrobas.GetUpgradedStarterRelic))]
+public static class PatchTouchOfOrobas
+{
+    public static void Postfix(RelicModel starterRelic, ref RelicModel __result)
+    {
+        if (starterRelic.Id == ModelDb.Relic<PitchBlack>().Id) __result = ModelDb.Relic<TotalDarkness>().ToMutable();
+    }
+}
