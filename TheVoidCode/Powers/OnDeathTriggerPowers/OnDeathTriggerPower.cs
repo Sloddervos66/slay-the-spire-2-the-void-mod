00ad2e214662c2de36397e7999363311daa4693e
Sloddervos66 <sloddervos66@gmail.com>
2026-03-21 11:25:34 +0100

    Added OnDeathTriggerPower an abstract class all OnDeath powers will derive from

---
+++ TheVoidCode/Powers/OnDeathTriggerPowers/OnDeathTriggerPower.cs
@@ -0,0 +1,24 @@
+﻿using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace TheVoid.TheVoidCode.Powers.OnDeathTriggerPowers;
+
+public abstract class OnDeathTriggerPower : TheVoidPower
+{
+    public override PowerType Type => PowerType.Debuff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterDeath(PlayerChoiceContext choiceContext, Creature creature, bool wasRemovalPrevented, float deathAnimLength)
+    {
+        if (creature == Owner)
+        {
+            for (var i = 0; i < Amount; i++)
+            {
+                await TriggerEffect(choiceContext);
+            }
+        }
+    }
+    
+    protected abstract Task TriggerEffect(PlayerChoiceContext choiceContext);
+}
