d8c65d667d6e7b1ac6f9c7d0d24722c9ae53abda
Sloddervos66 <sloddervos66@gmail.com>
2026-03-21 10:12:03 +0100

    Added Eclipse, a rare card with some placeholder "art"

---
+++ TheVoidCode/Cards/Rare/Eclipse.cs
@@ -0,0 +1,36 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using TheVoid.TheVoidCode.Character;
+using TheVoid.TheVoidCode.Powers;
+
+namespace TheVoid.TheVoidCode.Cards.Rare;
+
+[Pool(typeof(TheVoidCardPool))]
+public sealed class Eclipse : TheVoidCard
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<BlindPower>(5m)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<BlindPower>()];
+
+    public Eclipse()
+        : base(2, CardType.Skill, CardRarity.Rare, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+        
+        await PowerCmd.Apply<BlindPower>(target, DynamicVars["BlindPower"].IntValue, Owner.Creature, null);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["BlindPower"].UpgradeValueBy(5m);
+    }
+}
