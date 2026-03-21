c18a918d0d1d80c62765064bb8585df498f56b3f
Sloddervos66 <sloddervos66@gmail.com>
2026-03-21 14:16:26 +0100

    Added Unravel

---
+++ TheVoidCode/Cards/Common/Unravel.cs
@@ -0,0 +1,30 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheVoid.TheVoidCode.Character;
+
+namespace TheVoid.TheVoidCode.Cards.Common;
+
+[Pool(typeof(TheVoidCardPool))]
+public sealed class Unravel() : TheVoidCard(0, CardType.Skill, CardRarity.Common, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => 
+    [
+        new HpLossVar(4m),
+        new CardsVar(3)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.Damage(choiceContext, Owner.Creature, DynamicVars.HpLoss.BaseValue, ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move, this);
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.HpLoss.UpgradeValueBy(-1m);
+    }
+}
