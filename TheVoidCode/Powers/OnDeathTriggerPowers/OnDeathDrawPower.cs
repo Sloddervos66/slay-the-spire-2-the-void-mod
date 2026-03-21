16f72133492d10f7d3fc4636f5f5727cb2d74d31
Sloddervos66 <sloddervos66@gmail.com>
2026-03-21 11:25:57 +0100

    OnDeathDrawPower when an enemy dies, draw the amount of stacks of cards

---
+++ TheVoidCode/Powers/OnDeathTriggerPowers/OnDeathDrawPower.cs
@@ -0,0 +1,15 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace TheVoid.TheVoidCode.Powers.OnDeathTriggerPowers;
+
+public sealed class OnDeathDrawPower : OnDeathTriggerPower
+{
+    protected override async Task TriggerEffect(PlayerChoiceContext choiceContext)
+    {
+        var player = Applier?.Player;
+        if (player == null) return;
+        
+        await CardPileCmd.Draw(choiceContext, Amount, player);
+    }
+}
