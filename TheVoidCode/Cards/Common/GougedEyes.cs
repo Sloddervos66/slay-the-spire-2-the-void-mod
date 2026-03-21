dd87728c5c8b524dd23d2e7fd20998f0307a999f
Sloddervos66 <sloddervos66@gmail.com>
2026-03-21 13:46:29 +0100

    Added Gouged Eyes

---
+++ TheVoidCode/Cards/Common/GougedEyes.cs
@@ -0,0 +1,47 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheVoid.TheVoidCode.Character;
+using TheVoid.TheVoidCode.Powers;
+
+namespace TheVoid.TheVoidCode.Cards.Common;
+
+[Pool(typeof(TheVoidCardPool))]
+public sealed class GougedEyes() : TheVoidCard(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<BlindPower>()];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(7m, ValueProp.Move),
+        new DamageVar("BlindedDamage", 12m, ValueProp.Move)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+
+        var damage = HasBlind() ? DynamicVars["BlindedDamage"].BaseValue : DynamicVars.Damage.BaseValue;
+        await DamageCmd.Attack(damage).FromCard(this).Targeting(target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(2m);
+        DynamicVars["BlindedDamage"].UpgradeValueBy(4m);
+    }
+
+    private bool HasBlind()
+    {
+        var player = Owner.Creature;
+        var currentBlind = player.GetPower<BlindPower>();
+
+        return currentBlind is { Amount: > 0 };
+    }
+}
