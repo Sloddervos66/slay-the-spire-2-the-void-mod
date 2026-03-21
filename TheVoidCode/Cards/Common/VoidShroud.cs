f1c068c4825cf4f6ed719fb1968df9a6b47dff14
Sloddervos66 <sloddervos66@gmail.com>
2026-03-21 14:48:07 +0100

    Added hover tip for Blind

--- TheVoidCode/Cards/Common/VoidShroud.cs
+++ TheVoidCode/Cards/Common/VoidShroud.cs
@@ -2,6 +2,7 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 using TheVoid.TheVoidCode.Character;
@@ -12,6 +13,7 @@ namespace TheVoid.TheVoidCode.Cards.Common;
 [Pool(typeof(TheVoidCardPool))]
 public sealed class VoidShroud() : TheVoidCard(1, CardType.Skill, CardRarity.Common, TargetType.Self)
 {
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<BlindPower>()];
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
         new BlockVar(7m, ValueProp.Move),
