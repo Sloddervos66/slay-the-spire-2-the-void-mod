810fe2e0d467641ebb9b7a350a9d5f7028246982
Sloddervos66 <sloddervos66@gmail.com>
2026-03-21 11:36:41 +0100

    Added Hollow Strike

---
+++ TheVoidCode/Cards/Common/HollowStrike.cs
@@ -0,0 +1,42 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheVoid.TheVoidCode.Character;
+using TheVoid.TheVoidCode.Powers.OnDeathTriggerPowers;
+
+namespace TheVoid.TheVoidCode.Cards.Common;
+
+[Pool(typeof(TheVoidCardPool))]
+public class HollowStrike : TheVoidCard
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<OnDeathDrawPower>()];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(6m, ValueProp.Move),
+        new PowerVar<OnDeathDrawPower>(1m)
+    ];
+
+    public HollowStrike()
+        : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+        
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+        await PowerCmd.Apply<OnDeathDrawPower>(target, DynamicVars["OnDeathDrawPower"].IntValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3m);
+    }
+}
