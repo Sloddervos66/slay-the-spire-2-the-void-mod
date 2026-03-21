c36bfb39e8b2469af0c04de06731c7986cfab3cd
Sloddervos66 <sloddervos66@gmail.com>
2026-03-21 12:42:12 +0100

    Added void slash

---
+++ TheVoidCode/Cards/Common/VoidSlash.cs
@@ -0,0 +1,43 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheVoid.TheVoidCode.Character;
+using TheVoid.TheVoidCode.Powers.OnDeathTriggerPowers;
+
+namespace TheVoid.TheVoidCode.Cards.Common;
+
+[Pool(typeof(TheVoidCardPool))]
+public sealed class VoidSlash : TheVoidCard
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<OnDeathGainEnergyPower>()];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(8m, ValueProp.Move),
+        new PowerVar<OnDeathGainEnergyPower>(1m)
+    ];
+
+    public VoidSlash()
+        : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+        
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+        await PowerCmd.Apply<OnDeathGainEnergyPower>(target, DynamicVars["OnDeathGainEnergyPower"].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3m);
+    }
+}
