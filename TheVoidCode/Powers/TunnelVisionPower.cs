fe240d7b500b23ef0a32049bd8c3a3226456c060
Sloddervos66 <sloddervos66@gmail.com>
2026-03-21 15:16:26 +0100

    Added Tunnel Vision power. Gives 1 energy at the cost of blind every turn

---
+++ TheVoidCode/Powers/TunnelVisionPower.cs
@@ -0,0 +1,20 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace TheVoid.TheVoidCode.Powers;
+
+public sealed class TunnelVisionPower : TheVoidPower
+{
+    public override PowerType Type => PowerType.None;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        if (player != Owner.Player) return;
+
+        await PlayerCmd.GainEnergy(1, player);
+        await PowerCmd.Apply<BlindPower>(Owner, Amount, Owner, null);
+    }
+}
