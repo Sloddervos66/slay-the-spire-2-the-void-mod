a3e2dc29225d9208ad8e8f312893f9dc74ab1f07
Sloddervos66 <sloddervos66@gmail.com>
2026-03-23 14:15:15 +0100

    Removed consume test as it was a test. Added Eclipse Strike

---
+++ TheVoidCode/Cards/Uncommon/EclipseStrike.cs
@@ -0,0 +1,38 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheVoid.TheVoidCode.Character;
+using TheVoid.TheVoidCode.Powers.OnDeathTriggerPowers;
+
+namespace TheVoid.TheVoidCode.Cards.Uncommon;
+
+[Pool(typeof(TheVoidCardPool))]
+public sealed class EclipseStrike() : TheVoidCard(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<VoidFlashPower>()];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(7m, ValueProp.Move),
+        new PowerVar<VoidFlashPower>(1m)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+        
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(target)
+            .WithHitFx(DefaultAttackVfx)
+            .Execute(choiceContext);
+        await PowerCmd.Apply<VoidFlashPower>(target, DynamicVars[VoidFlashPower.Name].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3m);
+    }
+}
