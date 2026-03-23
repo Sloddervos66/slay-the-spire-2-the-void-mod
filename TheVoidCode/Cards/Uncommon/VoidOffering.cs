f8023fda55c2f0c656e7b9f182ef409f63bb8527
Sloddervos66 <sloddervos66@gmail.com>
2026-03-23 18:22:57 +0100

    Added Void Offering

---
+++ TheVoidCode/Cards/Uncommon/VoidOffering.cs
@@ -0,0 +1,44 @@
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using TheVoid.TheVoidCode.Character;
+using TheVoid.TheVoidCode.Powers.OnDeathTriggerPowers;
+
+namespace TheVoid.TheVoidCode.Cards.Uncommon;
+
+[Pool(typeof(TheVoidCardPool))]
+public sealed class VoidOffering() : TheVoidCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromKeyword(CardKeyword.Exhaust)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<VoidMendPower>(3m)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+
+        var selectedCard = (
+            await CardSelectCmd
+                .FromHand(prefs: new CardSelectorPrefs(CardSelectorPrefs.ExhaustSelectionPrompt, 1),
+                    context: choiceContext,
+                    player: Owner,
+                    filter: null,
+                    source: this
+                )
+        ).FirstOrDefault();
+        if (selectedCard != null)
+        {
+            await CardCmd.Exhaust(choiceContext, selectedCard);
+        }
+        await PowerCmd.Apply<VoidMendPower>(target, DynamicVars[VoidMendPower.Name].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars[VoidMendPower.Name].UpgradeValueBy(2m);
+    }
+}
