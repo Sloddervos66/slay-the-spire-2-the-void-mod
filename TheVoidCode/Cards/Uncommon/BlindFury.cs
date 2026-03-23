56ec7a2530bf835667b275322ff2160f2f93ec2a
Sloddervos66 <sloddervos66@gmail.com>
2026-03-23 17:50:57 +0100

    Added Blind Fury

---
+++ TheVoidCode/Cards/Uncommon/BlindFury.cs
@@ -0,0 +1,45 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using TheVoid.TheVoidCode.Character;
+using TheVoid.TheVoidCode.Extensions;
+using TheVoid.TheVoidCode.Localization.DynamicVars;
+using TheVoid.TheVoidCode.Powers;
+
+namespace TheVoid.TheVoidCode.Cards.Uncommon;
+
+[Pool(typeof(TheVoidCardPool))]
+public sealed class BlindFury() : TheVoidCard(2, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    private const string AdditionalDamageName = "AdditionalDamage";
+    private const decimal AdditionalDamagePerStack = 3m;
+    private const decimal StacksPerBonus = 2m;
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new BlindFuryDamageVar(5m, AdditionalDamagePerStack, StacksPerBonus),
+        new(AdditionalDamageName, AdditionalDamagePerStack),
+        new PowerVar<BlindPower>(StacksPerBonus)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+
+        var additionalDamage = Owner.Creature.HasBlind()
+            ? DynamicVars[AdditionalDamageName].BaseValue * (Owner.Creature.GetPowerAmount<BlindPower>() / DynamicVars[BlindPower.Name].BaseValue)
+            : 0m;
+        var totalDamage = DynamicVars[BlindFuryDamageVar.Name].BaseValue + additionalDamage;
+        await DamageCmd.Attack(totalDamage).FromCard(this).Targeting(target)
+            .WithHitFx(DefaultAttackVfx)
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars[BlindFuryDamageVar.Name].UpgradeValueBy(2m);
+    }
+}
---
+++ TheVoidCode/Localization/DynamicVars/BlindFuryDamageVar.cs
@@ -0,0 +1,43 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using TheVoid.TheVoidCode.Extensions;
+using TheVoid.TheVoidCode.Powers;
+
+namespace TheVoid.TheVoidCode.Localization.DynamicVars;
+
+public class BlindFuryDamageVar(decimal baseValue) : DynamicVar(Name, baseValue)
+{
+    public new const string Name = "BlindFury";
+    private readonly decimal _additionalDamagePerStack;
+    private readonly decimal _stacksPerBonus;
+
+    public BlindFuryDamageVar(decimal baseValue, decimal additionalDamagePerStack, decimal stacksPerBonus)
+        : this(baseValue)
+    {
+        _additionalDamagePerStack = additionalDamagePerStack;
+        _stacksPerBonus = stacksPerBonus;
+    }
+
+    public override void UpdateCardPreview(
+        CardModel card, 
+        CardPreviewMode previewMode, 
+        Creature? target, 
+        bool runGlobalHooks)
+    {
+        var owner = card.Owner?.Creature;
+        if (owner != null && owner.HasBlind())
+        {
+            var blindStacks = owner.GetPowerAmount<BlindPower>();
+            var bonus = _additionalDamagePerStack * (blindStacks / _stacksPerBonus);
+            PreviewValue = BaseValue + bonus;
+        }
+        else
+        {
+            PreviewValue = BaseValue;
+        }
+        
+        base.UpdateCardPreview(card, previewMode, target, runGlobalHooks);
+    }
+}
