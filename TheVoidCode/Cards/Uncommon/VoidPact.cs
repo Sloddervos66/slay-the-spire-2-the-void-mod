341b56aa97a20aebe8b75bd40b10fea54c0c6ea1
Sloddervos66 <sloddervos66@gmail.com>
2026-03-26 19:44:02 +0100

    Added Void Pact correctly

--- TheVoidCode/Cards/Uncommon/VoidPact.cs
+++ TheVoidCode/Cards/Uncommon/VoidPact.cs
@@ -2,12 +2,9 @@ using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
-using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 using TheVoid.TheVoidCode.Character;
-using TheVoid.TheVoidCode.Localization.DynamicVars;
 
 namespace TheVoid.TheVoidCode.Cards.Uncommon;
 
@@ -16,31 +13,25 @@ public sealed class VoidPact() : TheVoidCard(0, CardType.Skill, CardRarity.Uncom
 {
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new VoidPactVar(15m),
-        new EnergyVar(1)
+        new("PercentDamage", 15m),
+        new EnergyVar(1),
+        new CalculationBaseVar(0m),
+        new ExtraDamageVar(1m),
+        new CalculatedDamageVar(ValueProp.Move).WithMultiplier((card, _) => 
+            card.Owner.Creature.MaxHp * (card.DynamicVars["PercentDamage"].BaseValue / 100m))
     ];
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.ForEnergy(this)];
-
-    protected override void AddExtraArgsToDescription(LocString description)
-    {
-        var voidPactVar = (VoidPactVar)DynamicVars[VoidPactVar.Name];
-        description.Add("TargetSelected", voidPactVar.HasTarget);
-    }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        var percent = DynamicVars[VoidPactVar.Name].BaseValue / 100m;
-        var actualDamage = Owner.Creature.MaxHp * percent;
-        
-        await CreatureCmd.Damage(choiceContext, Owner.Creature, actualDamage, ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move, this);
-        await PlayerCmd.GainEnergy(DynamicVars.Energy.BaseValue, Owner);
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await CreatureCmd.Damage(choiceContext, Owner.Creature, DynamicVars.CalculatedDamage.PreviewValue,
+            ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move, this);
     }
 
     protected override void OnUpgrade()
     {
         DynamicVars.Energy.UpgradeValueBy(1m);
-        
         RemoveKeyword(CardKeyword.Exhaust);
         AddKeyword(CardKeyword.Ethereal);
     }
