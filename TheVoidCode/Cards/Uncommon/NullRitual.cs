868355b34879ee1b28fa78c097698818a03146c6
Sloddervos66 <sloddervos66@gmail.com>
2026-03-26 22:22:07 +0100

    Added Null Ritual

---
+++ TheVoidCode/Cards/Uncommon/NullRitual.cs
@@ -0,0 +1,41 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheVoid.TheVoidCode.Character;
+using TheVoid.TheVoidCode.Powers;
+
+namespace TheVoid.TheVoidCode.Cards.Uncommon;
+
+[Pool(typeof(TheVoidCardPool))]
+public sealed class NullRitual() : TheVoidCard(2, CardType.Skill, CardRarity.Uncommon, TargetType.AllEnemies)
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<BlindPower>()];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new HpLossVar(7m),
+        new PowerVar<BlindPower>(3m)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+
+        var combatState = CombatState;
+        if (combatState == null) return;
+        
+        await CreatureCmd.Damage(choiceContext, Owner.Creature, DynamicVars.HpLoss.BaseValue, ValueProp.Unblockable | ValueProp.Unpowered | ValueProp.Move, this);
+        
+        var enemies = combatState.Enemies.Where(e => e.IsAlive);
+        await PowerCmd.Apply<BlindPower>(enemies, DynamicVars[BlindPower.Name].BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.HpLoss.UpgradeValueBy(-3m);
+    }
+}
