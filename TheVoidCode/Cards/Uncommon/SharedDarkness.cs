7742f12d902aa11ec0905774b558bfa30906c018
Sloddervos66 <sloddervos66@gmail.com>
2026-03-27 15:03:03 +0100

    Added Shared Darkness

---
+++ TheVoidCode/Cards/Uncommon/SharedDarkness.cs
@@ -0,0 +1,39 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheVoid.TheVoidCode.Character;
+using TheVoid.TheVoidCode.Extensions;
+using TheVoid.TheVoidCode.Powers;
+
+namespace TheVoid.TheVoidCode.Cards.Uncommon;
+
+[Pool(typeof(TheVoidCardPool))]
+public sealed class SharedDarkness() : TheVoidCard(2, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<BlindPower>()];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(32m, ValueProp.Move)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var target = cardPlay.Target;
+        if (target == null) return;
+
+        var hasBlind = target.HasBlind();
+        var blindAmount = target.GetPowerAmount<BlindPower>();
+        
+        var result = await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(target)
+            .WithHitFx(DefaultAttackVfx)
+            .Execute(choiceContext);
+        
+        if (hasBlind) await PowerCmd.Apply<BlindPower>(Owner.Creature, blindAmount, target, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(8m);
+    }
+}
