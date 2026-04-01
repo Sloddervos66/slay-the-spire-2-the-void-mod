a5135bfad08562deba51ef29253df3c4c9e1cc0d
Sloddervos66 <sloddervos66@gmail.com>
2026-04-01 19:02:19 +0200

    Added Blood Contract

--- README.md
+++ README.md
@@ -148,7 +148,7 @@ Found a bug or have a suggestion? Open an [Issue](https://github.com/Sloddervos6
 | **Total Darkness**     | At the start of combat, apply 10 Blind to all enemies.                                         | Starter  | Y         |
 | **Shattered Lens**     | Blind now ticks down by 2 at the end of the turn.                                              | Common   | Y         |
 | **Umbral Carapace**    | Whenever you apply Blind, gain 2 Block.                                                        | Uncommon | Y         |
-| **Blood Contract**     | The first time each turn you lose HP from a card, draw 2 cards.                                | Uncommon | N         |
+| **Blood Contract**     | The first time each turn you lose HP from a card, draw 2 cards.                                | Uncommon | Y         |
 | **Grave Convergence**  | The first time each turn an enemy dies, trigger its on death effects twice.                    | Rare     | N         |
 | **Abyssal Threshold**  | Whenever you apply 50 Blind to an enemy, it gains 3 Consumption and Void Claimed.              | Rare     | N         |
 | **The Hungering Void** | Whenever you apply Blind to yourself, gain Strength for this turn equal to the amount applied. | Rare     | N         |
--- TheVoidCode/Relics/Uncommon/BloodContract.cs
+++ TheVoidCode/Relics/Uncommon/BloodContract.cs
@@ -1,8 +1,39 @@
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace TheVoid.TheVoidCode.Relics.Uncommon;
 
 public sealed class BloodContract : TheVoidRelic
 {
-    public override RelicRarity Rarity { get; }
+    public override RelicRarity Rarity => RelicRarity.Uncommon;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(2)];
+    private bool _hasDrawn = false;
+
+    public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props, Creature? dealer, CardModel? cardSource)
+    {
+        if (target == Owner.Creature && result.UnblockedDamage > 0 && target.CombatState?.CurrentSide == Owner.Creature.Side)
+        {
+            if (cardSource == null) return;
+            if (_hasDrawn) return;
+            
+            await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+            _hasDrawn = true;
+        }
+    }
+
+    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        if (side == CombatSide.Enemy)
+        {
+            _hasDrawn = false;
+        }
+
+        await Task.CompletedTask;
+    }
 }
