dd0a742225d75d86010a44d109e7ad6a42ed1bbd
Sloddervos66 <sloddervos66@gmail.com>
2026-04-02 15:08:10 +0200

    Added Abyssal Threshold

--- TheVoidCode/Relics/Rare/AbyssalThreshold.cs
+++ TheVoidCode/Relics/Rare/AbyssalThreshold.cs
@@ -1,8 +1,43 @@
+using Godot;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using TheVoid.TheVoidCode.Extensions;
+using TheVoid.TheVoidCode.Powers;
 
 namespace TheVoid.TheVoidCode.Relics.Rare;
 
 public sealed class AbyssalThreshold : TheVoidRelic
 {
-    public override RelicRarity Rarity { get; }
+    public override RelicRarity Rarity => RelicRarity.Rare;
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => 
+    [
+        HoverTipFactory.FromPower<BlindPower>(), 
+        HoverTipFactory.FromPower<ConsumptionPower>(),
+        HoverTipFactory.FromPower<VoidClaimedPower>()
+    ];
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<BlindPower>(50m),
+        new PowerVar<ConsumptionPower>(3m)
+    ];
+
+    public override async Task AfterPowerAmountChanged(PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
+    {
+        if (power is not BlindPower) return;
+
+        var target = power.Owner;
+        if (!target.IsMonster || 
+            !target.HasBlind() || 
+            target.HasVoidClaimed() ||
+            target.GetPowerAmount<BlindPower>() < DynamicVars[BlindPower.Name].BaseValue
+        ) return;
+        
+        Flash();
+        await PowerCmd.Apply<ConsumptionPower>(target, DynamicVars[ConsumptionPower.Name].BaseValue, Owner.Creature, null);
+        await PowerCmd.Apply<VoidClaimedPower>(target, 1, Owner.Creature, null);
+    }
 }
